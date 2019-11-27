d84624b9adb88d98274ac24be17d9fa4a539a540
akuleshov7 <kul92@list.ru>
2019-11-27 12:23:38 +0300

    PCS-2: standardizing test naming for all samples and making changes in test driver to support that

--- yapftests/huaweistyle/huawei_code_style.py
+++ yapftests/huaweistyle/huawei_code_style.py
@@ -1,30 +1,37 @@
 import glob
 import os
 import unittest
-import re
 
 from yapf.yapflib import yapf_api
 
 
 class RunMainTest(unittest.TestCase):
-    TEST_PATTERN = re.compile(r'.*[0-9]+_[0-9]+_a_.*')
+    INCORRECT = '_incorrect_'
+    CORRECT = '_correct_'
+    RESOURCES_PATH = 'resources'
 
-    def __get_filename(self):
-        return 0
+    def __find_corresponding_result(self, test_filename):
+        """All test files should have prefix INCORRECT, all expected result files should have prefix CORRECT"""
+        expected_file_path = test_filename.replace(self.INCORRECT, self.CORRECT)
+        with open(expected_file_path, 'r') as file:
+            expected_str = file.read()
+        return expected_str
 
-    def __find_corresponding_result(self, test_filename, all_test_files):
-        base_filename = test_filename.replace('_a_', '_b_')
-        if base_filename in all_test_files:
-          return base_filename
-        else:
-          raise Exception('Not able to find ' + base_filename + ' in ' + '/resource directory')
+    def __get_tested_str(self):
+        """Run through resource path and run yapf on __incorrect__ files that should be fixed"""
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.RESOURCES_PATH)
+        all_test_files = glob.glob(os.path.join(path, '**', '*' + self.INCORRECT + '.py'), recursive=True)
+        test_set = set()
 
-
-    def test(self):
-        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources')
-        all_test_files = [f for f in glob.glob(path + "**/*.py", recursive=True)]
         for test in all_test_files:
-            self.__find_corresponding_result(test, all_test_files)
-            if self.TEST_PATTERN.match(test):
-                # FixMe: finish comparing tests
-                yapf_api.FormatFile(test)[0]
+            expected_str = self.__find_corresponding_result(test)
+            test_name_str = os.path.basename(test)
+            test_str = yapf_api.FormatFile(test)[0]
+
+            test_set.add((test_name_str, test_str, expected_str))
+        return sorted(test_set)
+
+    def test_run(self):
+        for test_name, test, expected in self.__get_tested_str():
+            with self.subTest(msg=test_name):
+                self.assertEqual(test, expected)
