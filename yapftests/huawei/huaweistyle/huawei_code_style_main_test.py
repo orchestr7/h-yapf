5852523805982ef9ec98645dd58e7a283a5dcd17
akuleshov7 <kul92@list.ru>
2019-12-09 20:05:41 +0800

    yapftests/huawei: move all yapf-ing into subtests
    All API usage should be done within subtest so that errors could be
    properly handled in the right place. It also allow to separate different
    subtests preventing them from interfering with each other. For instance,
    before this patch "warning" test would not work correctly, because
    "stderr" would always contain the warning message generated by the last loaded
    test case, not the currently checked one as one could exect.

--- yapftests/huawei/huaweistyle/huawei_code_style_main_test.py
+++ yapftests/huawei/huaweistyle/huawei_code_style_main_test.py
@@ -5,12 +5,17 @@ Copyright Information: Huawei Technologies Co., Ltd. All Rights Reserved © 2010
 Change History: 2019-11-26 18:23 Created
 """
 
+import contextlib
 import glob
 import os
 import sys
 
+from yapf.yapflib import yapf_api
+from yapf.yapflib import style
 from yapftests import yapf_test_helper
-from yapftests.huawei.huaweistyle.test_set import TestSet
+
+WINDOWS_EOL = '\r\n'
+UNIX_EOL = '\n'
 
 
 class RunMainTest(yapf_test_helper.YAPFTest):
@@ -20,9 +25,17 @@ class RunMainTest(yapf_test_helper.YAPFTest):
     MSG_FILE = '.msg'
     RESOURCES_PATH = 'resources'
 
+
     def test(self):
-        for test_name, test, expected in self.__get_test_sets():
+        for test_path, expected_path in self.__get_test_sets():
+            test_name = os.path.basename(test_path)
+
             with self.subTest(msg=test_name):
+                test = yapf_api.FormatFile(test_path)[0]
+                test = test.replace(WINDOWS_EOL, UNIX_EOL)
+
+                expected = self.__read_source(expected_path)
+
                 # test checks warnings (stderr)
                 if self.WARN in test_name:
                     self.assertCodeEqual(expected, sys.stderr.get)
@@ -31,7 +44,28 @@ class RunMainTest(yapf_test_helper.YAPFTest):
                 if self.INCORRECT in test_name:
                     self.assertCodeEqual(expected, test)
 
-    def setUp(self):
+
+    @classmethod
+    def setUpClass(cls):
+        # We could as well pass `style_config='huawei'` to `FormatFile()`
+        # but configuring via `setUpClass()` is the way used in other tests
+        # (such as "reformatter_facebook_test").
+        # Besides, even if we did passed `style_config`, `FormatFile()`
+        # would call the very same `style.SetGlobalStyle()` inside itself
+        # anyway.
+        #
+        style.SetGlobalStyle(style.CreateHuaweiStyle())
+
+
+    @contextlib.contextmanager
+    def subTest(self, msg, **params):
+        self.setUpSubtest()
+        with super().subTest(msg, **params):
+            yield
+        self.tearDownSubtest()
+
+
+    def setUpSubtest(self):
         class RedirectedStdErr:
             get = ''
 
@@ -41,8 +75,11 @@ class RunMainTest(yapf_test_helper.YAPFTest):
         self.__prev_state = sys.stderr
         sys.stderr = RedirectedStdErr()
 
-    def tearDown(self):
+
+    def tearDownSubtest(self):
         sys.stderr = self.__prev_state
+        del self.__prev_state
+
 
     def __find_matching_resources(self, pattern):
         path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
@@ -50,17 +87,34 @@ class RunMainTest(yapf_test_helper.YAPFTest):
         return glob.glob(os.path.join(path, '*', f'*{pattern}.py'),
                          recursive=True)
 
+
     def __get_test_sets(self):
         """Create test sets for all test cases:
         1) that are expected to be fixed (_incorrect_)
         2) that are expected to generate a warning (_warning_)
          """
-        test_set_res = TestSet()
+        subtests = []
 
-        all_test_fixes = self.__find_matching_resources(self.INCORRECT)
-        test_set_res.enrich(all_test_fixes, self.INCORRECT, self.CORRECT)
+        subtests.extend(self.__find_tests(self.INCORRECT, self.CORRECT))
+        subtests.extend(self.__find_tests(self.WARN, self.MSG_FILE,
+            replace_suffix='.py'))
 
-        all_test_warn = self.__find_matching_resources(self.WARN)
-        test_set_res.enrich(all_test_warn, self.WARN + '.py', self.MSG_FILE)
+        return sorted(subtests)
 
-        return sorted(test_set_res.test_set)
+
+    def __find_tests(self, test_pattern, expected_pattern, replace_suffix=''):
+        test_paths = self.__find_matching_resources(test_pattern)
+
+        def get_expected_path(test_path):
+            return test_path.replace(f'{test_pattern}{replace_suffix}',
+                expected_pattern)
+        expected_paths = map(get_expected_path, test_paths)
+
+        return zip(test_paths, expected_paths)
+
+
+    def __read_source(self, path):
+        # note that python 3 reads in "universal new line" mode by default,
+        # i.e. any EOL markers ('\n', '\r', '\r\n') will be replaced with '\n'
+        with open(path, 'r') as file:
+            return file.read()
